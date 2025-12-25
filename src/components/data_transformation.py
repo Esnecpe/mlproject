58450721e5c03ea80a9928ecc8c20524a510cef7
Esnecpe <esnecpe356@gmail.com>
2025-12-25 00:23:27 -0800

    changes

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts', "proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -28,7 +28,7 @@ class DataTransformation:
         
         '''
         try:
-            numerical_columns = ['writing score', 'reading score']
+            numerical_columns = ["writing score", "reading score"]
             categorical_columns = [
                 "gender",
                 "race_ethnicity", 
@@ -41,17 +41,17 @@ class DataTransformation:
 
                 steps=[
                 ("imputer",SimpleImputer(strategy="median")),
-                ("scalar",StandardScaler())
+                ("scaler",StandardScaler())
 
                 ]
             )    
 
-            cat_pipeline=Pipeline(
+            cat_pipeline= Pipeline(
 
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
 
             )
@@ -87,7 +87,7 @@ class DataTransformation:
             preprocessing_obj=self.get_data_transformer_object()
 
             target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["writing score", "reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -104,7 +104,7 @@ class DataTransformation:
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-            
+
             logging.info(f"Saved preprocessing object.")
 
             save_object(
