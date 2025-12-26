8182d79c6045604778bf72af3c0e9a81d0fab002
Esnecpe <esnecpe356@gmail.com>
2025-12-25 16:22:56 -0800

    Files Updates

--- artifacts/prompt_snapshot_model_trainer_2025-12-25_000000.py
+++ artifacts/prompt_snapshot_model_trainer_2025-12-25_000000.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,7 +26,6 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -41,10 +40,10 @@ class ModelTrainer:
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Linear regression": LinearRegression(),
+                "XGBClassifier": XGBRegressor(),
+                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
+                "AdaBoost Classifier": AdaBoostRegressor(),
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
@@ -55,28 +54,27 @@ class ModelTrainer:
 
             ## To get best model name from dict
 
-            best_model_name = list(model_report.keys())[
+            best_model_name = list(model_report.keys())[ 
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            if best_model_score < 0.6:
+                raise  CustomException ("No best model found")
+            
+            logging.info(f"Best model found on both training and testing dtaset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
+
             )
 
-            predicted=best_model.predict(X_test)
+            predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
 
 
-
-            
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
